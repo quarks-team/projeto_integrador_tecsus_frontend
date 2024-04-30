8f093f4fdc6266035c14e1fa050b5da8dc137b25
Felipe Sobral <felipesobral_@hotmail.com>
2024-04-30 12:58:01 -0300

    feat: implement access to the PowerBi views through the HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,36 @@
     <transition name="fade" mode="out-in">
         <div v-if="isVisible" class="container-principal">
             <h1>Dashboards</h1>
+            <div class="container-dash">
+                <DashAgua />
+                <DashLuz />
+            </div>
         </div>
     </transition>
 </template>
 
 <script lang="ts">
+import DashAgua from '@/components/DashAgua.vue';
+import DashLuz from '@/components/DashLuz.vue';
+
 export default {
+  
   data() {
     return {
       isVisible: false
     };
   },
+
   mounted() {
     setTimeout(() => {
       this.isVisible = true;
     }, 1000);
-  }
+  },
+
+  components: {
+    DashAgua, DashLuz
+  },
+
 }
 </script>
 
@@ -30,11 +44,20 @@ export default {
     margin-top: 3%;
     margin-right: 4%;
     margin-bottom: 3%;
+    display: flex;
+    flex-direction: column;
+    gap: 4.5rem;
 }
 
 .container-principal h1 {
     color: #776AE3;
     font-size: 6vmin;
     font-weight: 400;
+}
+
+.container-dash {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2%;
 }
 </style>
---
+++ src/views/PowerBiLuz.vue
@@ -0,0 +1,46 @@
+<template>
+    <transition name="fade" mode="out-in">
+        <div v-if="isVisible" class="container-powerbi">
+            <h1>Dashboard de Luz</h1>
+            <!-- <iframe></iframe>  --> 
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+export default {
+
+  data() {
+    return {
+      isVisible: false
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.isVisible = true;
+    }, 1000);
+  }
+  
+}
+</script>
+
+<style>
+@import "../assets/base.css";
+
+.container-powerbi {
+  margin-left: 4%;
+  margin-top: 3%;
+  margin-right: 4%;
+  margin-bottom: 3%;
+  display: flex;
+  flex-direction: column;
+  gap: 4.5rem;
+}
+
+.container-powerbi h1 {
+  color: #776AE3;
+  font-size: 6vmin;
+  font-weight: 400;
+}
+</style>
