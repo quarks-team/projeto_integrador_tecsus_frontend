5e8bf41a98576105730d7e51690a020ece7d40c3
Felipe Sobral <felipesobral_@hotmail.com>
2024-04-30 12:56:39 -0300

    feat: create components for Dashboards access

---
+++ src/components/DashLuz.vue
@@ -0,0 +1,69 @@
+<template>
+    <transition name="fade" mode="out-in">
+        <div v-if="isVisible" class="container-card">
+            <img src="../assets/luz.png">
+            <h2>Dashboard de contas de Luz</h2>
+            <RouterLink to="/luz">ACESSE AQUI</RouterLink>
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+
+export default {
+  data() {
+    return {
+      isVisible: false
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isVisible = true;
+    }, 1000);
+  }
+}
+</script>
+
+
+<style>
+@import "../assets/base.css";
+
+.container-card {
+    display: flex;
+    flex-direction: column;
+    gap: 5rem;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #88E570;
+    box-shadow: 2% 2% 50% 5% #C0C0C0;
+    border-radius: 15px;
+    background-color: white;
+}
+
+.container-card img {
+    padding-top: 5%;
+    width: 20%;
+    height: auto;
+}
+
+.container-card h2 {
+    font-size: 1.5em;
+    font-weight: 200;
+    color: black;
+}
+
+.container-card a {
+    text-decoration: none;
+    color: #88E570;
+    font-weight: 400;
+    cursor: pointer;
+    font-size: 1em;
+    padding-bottom: 10%;
+}
+
+.container-card a:hover {
+    transition: all 1s;
+    font-size: 1.2em;
+}
+</style>
