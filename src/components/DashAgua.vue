95cf70faeebaf604cb71da3e1fd54ded621a8641
guilherme0066 <cursioalves@gmail.com>
2024-05-17 20:40:06 -0300

    refactor:FinishingResponsivityAdjusts

--- src/components/DashAgua.vue
+++ src/components/DashAgua.vue
@@ -6,12 +6,8 @@
       title="Acessar Dash de Água"
       @click.prevent="dashRedirect()"
     >
-    <div class = "img-container">
       <img src="../assets/icons/agua.png" />
-      </div>
-      <div>
       <h2>Água</h2>
-    </div>
       <!-- <RouterLink to="/agua">ACESSE AQUI <p>☞</p></RouterLink> -->
     </div>
   </transition>
@@ -55,35 +51,31 @@ export default {
   align-items: center;
   border: 2px solid rgb(0, 80, 252);
   box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-  transition-duration: 2s;
-  /* border-radius: 15px; */
+  border-radius: 15px;
+  position: relative;
   width: min(35vw, 75vh);
-  height: min(35vw, 30vh);
+  height: min(35vw, 75vh);
   cursor: pointer;
   background-color: var(--branco-auxiliar);
   border: 1px solid var(--cinza-auxiliar-0-5);
 }
 
-.container-card-agua {
+/* .container-card-agua {
   flex-direction: row-reverse;
   justify-content: flex-start;
   padding-right: 5%;
-}
-.container-card-agua .img-container{
-  
-}
-.container-card-agua .img-container img {
-  width: 80%;
+} */
+
+.container-card-agua img {
+  width: 40%;
   /* flex-direction: column; */
-  height: 80%;
-  object-fit: fill;
+  height: auto;
   opacity: 1;
   border-radius: 50%;
   padding-left: 5%;
   padding-right: 5%;
   padding-top: 5%;
   padding-bottom: 5%;
- 
   /* margin-top: 50%; */
   background-color: var(--branco-auxiliar-0-8);
   border: 2px solid var(--azul-light-0-8);
@@ -124,26 +116,22 @@ export default {
 }
 
 .container-card-agua:hover {
-  /* transition: all 0.1s; */
+  transition: all 0.1s;
   font-size: 1.2em;
   border: 1px solid rgb(0, 80, 252);
-  transition-duration: 1.5s;
-  box-shadow: 3px 3px 10px rgba(0, 80, 252, 0.2);
-  width: min(35vw, 80vh);
-  height: min(35vw, 35vh);
 }
 
 .container-card-agua:hover {
-  /* background-color: var(--azul-contraste-0-6); */
-  /* background-image:linear-gradient(rgba(255,255,255,1), rgba(255,255,255,1)), url("../assets/dash_card_bg/bg_agua1.webp"); */
+  background-color: var(--azul-contraste-0-6);
+  background-image:url('../assets/dash_card_bg/bg_agua2.webp');
   background-blend-mode: multiply;
-  /* transition: background-image 3s; */
+  transition: background-image 3s;
 }
 
 .container-card-agua:hover img {
   background-color: var(--branco-auxiliar-0-6);
   border: 3px solid var(--verde-contraste);
   /* opacity: 0.1; */
-  /* transition: all 0.1s; */
+  transition: all 0.1s;
 }
 </style>
