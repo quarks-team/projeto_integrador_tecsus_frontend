b30e3de86c07da579a14a9c534fc3750480a5b7a
Felipe Sobral <felipesobral_@hotmail.com>
2024-06-09 06:40:07 -0300

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Verify wether .env file already exist
+# Environment Variable that will be configurate in pipeline
+SONAR_TOKEN=$1
+
+# Verifying whether .env file already exist
 if [ -f .env ]; then
   echo ".env file already exists. Skipping creation."
+  echo "::set-output name=env_created::false"
 else
-  # Environment Variable that will be configurated in pipeline
-  SONAR_TOKEN=$1
-
-  # Create .env file with SONAR_TOKEN from secrets
+  # Creating .env file with SONAR_TOKEN var
   echo "SONAR_TOKEN=$SONAR_TOKEN" > .env
 
   echo ".env file has been created."
+  echo "::set-output name=env_created::true"
 fi
