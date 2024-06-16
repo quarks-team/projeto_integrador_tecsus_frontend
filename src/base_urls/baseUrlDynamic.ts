86444d88630d356b9115009bdf09093f71fc9ad7
Felipe Sobral <felipesobral_@hotmail.com>
2024-06-16 17:45:54 -0300

    Update baseUrlDynamic.ts

--- src/base_urls/baseUrlDynamic.ts
+++ src/base_urls/baseUrlDynamic.ts
@@ -1,23 +1,41 @@
 import axios from 'axios'
 
 // Determinando o baseURL dinamicamente
-const getBaseUrl = () => {
-  if (window.location.hostname === 'billing-ingestion-production.up.railway.app' || 
-      window.location.hostname === 'quarks-team.github.io') {
-    return 'https://billing-ingestion-production.up.railway.app';
-  } else {
-    return 'http://localhost:3000';
+const checkLocalBackend = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/alerts');
+    if (response.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
   }
-}
+};
+
+const getBaseUrl = async () => {
+  const isLocalBackendAvailable = await checkLocalBackend();
+  if (isLocalBackendAvailable) {
+    return 'http://localhost:3000';
+  } else {
+    return 'https://billing-ingestion-production.up.railway.app'; 
+  }
+};
+
+getBaseUrl().then(baseUrl => {
+  console.log('Base URL:', baseUrl);
+});
 
 
 // Definindo a URL base para as requisições
 const tecsusAPI = axios.create({
-  baseURL: getBaseUrl(),
+  baseURL: await getBaseUrl(),
   withCredentials: false, 
   headers: {
     'Content-Type': 'application/json',
-    Accept: 'application/json'
+    Accept: 'application/json',
+    'Access-Control-Allow-Origin': '*'
   }
 })
 
